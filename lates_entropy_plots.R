8a4f113777a11214264249deab9f69cf42508aa7
Jessica Rick <jrick@uwyo.edu>
2021-01-13 21:03:27 -0700

    more scripts

---
+++ lates_entropy_plots.R
@@ -0,0 +1,193 @@
+library(rhdf5)
+library(abind)
+library(RColorBrewer)
+library(tidyverse)
+library(memisc)
+colors <- brewer.pal(3, "Set3")
+
+## Accompanying metadata, change for your project
+details <- read.csv("../data/lates_combined_all_info_dnaID_entropy.csv",
+                    header=T)
+
+for (i in 2:6){
+  k <- 3
+  spp <- "lsta"
+  
+  names <- read.table(paste0("../data/admixture/",spp,".ind"), header=F)
+  colnames(names) <- c("names")
+  fishinfo <- left_join(names, details, by.x="names", by.y="names",
+                        all.x=T, all.y=F)
+  
+  ## Extract relevant parameters from .hdf5 format
+  data1.q <- h5read(paste("../data/admixture/",spp,"_092320_0.5_maf0.01_thin90.entropy.k",k,".rep1.hdf5",sep=""), "q")
+  data2.q <- h5read(paste("../data/admixture/",spp,"_092320_0.5_maf0.01_thin90.entropy.k",k,".rep2.hdf5",sep=""), "q")
+  data3.q <- h5read(paste("../data/admixture/",spp,"_092320_0.5_maf0.01_thin90.entropy.k",k,".rep3.hdf5",sep=""), "q")
+  
+  ## Combine chains for each parameter estimated, take mean
+  allq <- abind(data1.q,data2.q,data3.q,along=1)
+  q <- t(apply(allq, 2:3, mean))
+  
+  ## Get credible intervals and calculate mean
+  q.ci <- apply(allq, 2:3, quantile, probs=c(0.025,0.975))
+  q.ci.width <- q.ci[2,2,]-q.ci[1,2,]
+  print(mean(q.ci.width))
+  print(median(q.ci.width))
+  
+  ## Plot point estimates for q
+  # pdf("q_hist_lsta_k2.pdf", width=6.5, height=9)
+  # par(mfrow=c(2,2))
+  # for(i in 1:length(unique(fishinfo$Field.ID))){
+  #   hist(q[which(fishinfo$Field.ID ==
+  #                unique(fishinfo$Field.ID)[i]),1], 
+  #        xlab="proportion of ancestry (q)", 
+  #        main=unique(fishinfo$Field.ID)[i],
+  #        col="gray90", xlim=c(0,1))
+  # }
+  # dev.off()
+  
+  # pdf(paste("../../lates_popgen/results/",spp,"_conv_plots_k",k,".pdf",sep=""))
+  # par(mfrow=c(4,4))
+  # for (i in 1:nrow(names)){
+  #   plot(data1.q[,1,i],type="l",main=names[i,1],ylim=c(0,1))
+  #   lines(data2.q[,1,i],col="blue")
+  #   lines(data3.q[,1,i],col="red")
+  # }
+  # dev.off()
+  
+  
+  # stacked barplot
+  q.df <- data.frame(names=fishinfo$names,
+                     site=factor(as.character(fishinfo$site), 
+                                 levels=c("Kagunga","Kigoma","N_Mahale","S_Mahale","Isonga","Ikola","Mpinbwe","Kirando","Wampembe","Kasanga","Cameroon","Congo","Dar")),
+                     spp=factor(fishinfo$entropy_dnaID),
+                     #lib=factor(fishinfo$Library),
+                     #plate=factor(fishinfo$Plate),
+                     #sex=fishinfo$Sex,
+                     q[,1:k]
+  )
+  
+  q.df$assign <- factor(case_when(q.df$X1 > 0.6 ~ "q1",
+                           q.df$X2 > 0.6 ~ "q2",
+                           #q.df$X3 > 0.6 ~ "q3",
+                           #q.df$X4 > 0.6 ~ "q4",
+                           # q.df$X5 > 0.6 ~ "q5",
+                           TRUE ~ "none"),
+                        levels=c("q1","q2",
+                                 #"q3",#"q4",#"q5",#"q6",
+                                 "none"))
+  
+  q.long <- gather(q.df,group,value,
+                   colnames(q.df)[4:(k+3)])
+  
+  
+  plot <- ggplot(q.long[!is.na(q.long$site),], 
+                 aes(fill=group, y=value, x=names)) + 
+    geom_bar(position="stack", stat="identity", width=1) +
+    facet_grid(~site, drop=TRUE, scales="free_x", shrink=TRUE, space="free",switch="x") +
+    #facet_grid(~assign,drop=TRUE,space="free",scales="free_x",shrink=TRUE, switch="x") +
+    #facet_grid(vars(site),drop=TRUE,space="free",scales="free_x",shrink=TRUE) +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_text(size=6,angle=90),
+          axis.ticks.x=element_blank(),
+          panel.background = element_blank(),
+          strip.text.x = element_text(angle=90,size=12),
+          strip.text.y = element_text(size=18),
+          legend.position="none",
+          axis.text.y=element_text(size=12),
+          axis.title.y=element_blank(),
+          legend.text=element_text(size=16)) 
+  print(plot)
+}
+
+
+##########################
+## all species combined ##
+##########################
+
+k <- 4
+
+names <- read.csv(paste0("../data/admixture/lates_all.ind"), header=T)
+colnames(names) <- c("names","pop","taxon")
+fishinfo <- left_join(names, details, by.x="names", by.y="names",
+                      all.x=T, all.y=F)
+
+## Extract relevant parameters from .hdf5 format
+data1.q <- h5read(paste("../../../lates_popgen/data/admixture/lates_all_092320_0.5_maf0.01_thin90.entropy.k",k,".rep1.hdf5",sep=""), "q")
+data2.q <- h5read(paste("../../../lates_popgen/data/admixture/lates_all_092320_0.5_maf0.01_thin90.entropy.k",k,".rep2.hdf5",sep=""), "q")
+data3.q <- h5read(paste("../../../lates_popgen/data/admixture/lates_all_092320_0.5_maf0.01_thin90.entropy.k",k,".rep3.hdf5",sep=""), "q")
+
+## Combine chains for each parameter estimated, take mean
+allq <- abind(data1.q,data2.q,data3.q,along=1)
+q <- t(apply(allq, 2:3, mean))
+
+## Get credible intervals and calculate mean
+q.ci <- apply(allq, 2:3, quantile, probs=c(0.025,0.975))
+q.ci.width <- q.ci[2,2,]-q.ci[1,2,]
+print(mean(q.ci.width))
+print(median(q.ci.width))
+
+## Plot point estimates dsfor q (TO DO: add code for q/Q plot)
+# pdf("q_hist_lsta_k2.pdf", width=6.5, height=9)
+# par(mfrow=c(2,2))
+# for(i in 1:length(unique(fishinfo$Field.ID))){
+#   hist(q[which(fishinfo$Field.ID ==
+#                unique(fishinfo$Field.ID)[i]),1], 
+#        xlab="proportion of ancestry (q)", 
+#        main=unique(fishinfo$Field.ID)[i],
+#        col="gray90", xlim=c(0,1))
+# }
+# dev.off()
+
+# pdf(paste("../../lates_popgen/results/",spp,"_conv_plots_k",k,".pdf",sep=""))
+# par(mfrow=c(4,4))
+# for (i in 1:nrow(names)){
+#   plot(data1.q[,1,i],type="l",main=names[i,1],ylim=c(0,1))
+#   lines(data2.q[,1,i],col="blue")
+#   lines(data3.q[,1,i],col="red")
+# }
+# dev.off()
+
+
+# stacked barplot
+q.df <- data.frame(names=fishinfo$names,
+                   site=factor(as.character(fishinfo$site), 
+                               levels=c("Kagunga","Kigoma","N_Mahale","S_Mahale","Isonga","Ikola","Mpinbwe","Kirando","Wampembe","Kasanga","Cameroon","Congo","Dar")),
+                   spp=factor(fishinfo$entropy_dnaID),
+                   #lib=factor(fishinfo$Library),
+                   #plate=factor(fishinfo$Plate),
+                   #sex=fishinfo$Sex,
+                   q[,1:k]
+)
+
+# q.df$assign <- factor(case_when(q.df$X1 > 0.6 ~ "q1",
+#                          q.df$X2 > 0.6 ~ "q2",
+#                          q.df$X3 > 0.6 ~ "q3",
+#                          q.df$X4 > 0.6 ~ "q4",
+#                          q.df$X5 > 0.6 ~ "q5",
+#                          TRUE ~ "none"),
+#                       levels=c("q1","q2","q3","q4","q5","q6","none"))
+
+q.long <- gather(q.df,group,value,
+                 colnames(q.df)[4:(k+3)])
+
+colors2<-c("#8DD3C7","#BEBADA","#FB8072","#80B1D3",
+           "#B3DE69","#FCCDE5","#D9D9D9","#BC80BD","#CCEBC5","#ffbc42")
+
+plot <- ggplot(q.long[!is.na(q.long$site),], 
+               aes(fill=group, y=value, x=names)) + 
+  geom_bar(position="stack", stat="identity", width=1) +
+  #facet_grid(~site, drop=TRUE, scales="free_x", shrink=TRUE, space="free",switch="x") +
+  facet_grid(~spp,drop=TRUE,space="free",scales="free_x",shrink=TRUE, switch="x") +
+  #facet_grid(vars(site),drop=TRUE,space="free",scales="free_x",shrink=TRUE) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        panel.background = element_blank(),
+        strip.text.x = element_text(angle=90,size=12),
+        strip.text.y = element_text(size=18),
+        legend.position="none",
+        axis.text.y=element_text(size=12),
+        axis.title.y=element_blank(),
+        legend.text=element_text(size=16)) +
+  scale_fill_manual(values=colors2[c(3,1,2,10)])
+print(plot)
